44324ab86a79cc26a431a41066e83c6747d8573e
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-14 16:29:02 +0200

    set an array for "frigorifero" with all' the fruits as elements

---
+++ array-lista/Js/script.js
@@ -0,0 +1,5 @@
+// creo un array che rappresentera' il frigorifero con la frutta che ho a disposizione
+
+
+const frigorifero =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
+
