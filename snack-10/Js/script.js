b98bc924dd94a9d2ff8cb7141442ee318dc4ca36
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-15 12:27:09 +0200

    new test with a longer array.

--- snack-10/Js/script.js
+++ snack-10/Js/script.js
@@ -1,7 +1,7 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-let array1 = ["1", "2", "3", "4", "5"];
+let array1 = ["1", "2", "3", "4", "5", "6", "7", "8"];
 let array2 = ["1", "2", "3"];   
 
 
