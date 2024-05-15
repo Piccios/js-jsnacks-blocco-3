981f7c4d3fc1fa17c6a95248a887acd0f98bfe46
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-15 12:17:59 +0200

    first test

--- snack-10/Js/script.js
+++ snack-10/Js/script.js
@@ -1,6 +1,12 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-let array1 = [];
-let array2 = [];   
+let array1 = ["a", "b", "c", "d", "e"];
+let array2 = ["a", "b", "c"];   
+
+while (array1.length < array2.length) {
+    array1.push(array2.pop())
+}
+
+console.log(array1 + array2)
 
