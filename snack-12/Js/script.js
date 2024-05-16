c561d4c32567775468d0dc209e9ccc68fb39d189
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-16 11:53:18 +0200

    correzione

--- snack-12/Js/script.js
+++ snack-12/Js/script.js
@@ -1,10 +1,20 @@
-const word1 = prompt("Enter a word");
-const word2 = prompt("Enter another word");
+const userWord1 = prompt("Enter a word");
+const userWord2 = prompt("Enter another word");
 
-
+if (sameLength(userWord1, userWord2)) {
+    console.log(userWord1, userWord2);
+} else {
+    if (userWord1.length > userWord2.length) {
+        console.log(userWord1);
+    } else {
+        console.log(userWord2);
+    };
+}
 
 function sameLength(word1, word2) {
     if (word1.length === word2.length) {
-        return (word1, word2);
+        return true;
+    } else {
+        return false;
     }
 }
