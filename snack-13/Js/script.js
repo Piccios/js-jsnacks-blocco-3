ba12090ffad27e88b21bb711c975b3bcb223a263
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-16 12:23:46 +0200

    non riesco ad implementare il ciclo for con l'operazione che voglio eseguire

---
+++ snack-13/Js/script.js
@@ -0,0 +1,10 @@
+
+
+function invert(number) {
+    let result = 0;
+    const stringNum = number.toString();
+    const length = stringNum.length;
+
+    for (let i = length - 1; i >= 0; i--) {
+    }
+}
