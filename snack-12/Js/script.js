b292cbf9bf596fac6df8e9b9e4250e8aea67e7ba
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-16 11:44:22 +0200

    console non ritorna niente

--- snack-12/Js/script.js
+++ snack-12/Js/script.js
@@ -5,12 +5,6 @@ const word2 = prompt("Enter another word");
 
 function sameLength(word1, word2) {
     if (word1.length === word2.length) {
-        console.log(word1, word2);
-    } else {
-        if (word1.length > word2.length) {
-            console.log(word1);
-        } else {
-            console.log(word2);
-        }
+        return (word1, word2);
     }
 }
