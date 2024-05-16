79d97842e694dcbd871653a01f9b7bfbf232f70d
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-16 11:28:59 +0200

    primo test

--- snack-12/Js/script.js
+++ snack-12/Js/script.js
@@ -3,6 +3,11 @@ const word2 = prompt("Enter another word");
 
 
 function wordLength(word1, word2){
+    if (word1.length = word2.length){
+        return (word1 + word2);
+    if (word1.length > word2.length)
+        return (word1);
+    else return (word2);
+}
 
-    
 }
