37c2fee505e168816efdd4d5a7d089b621273657
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-16 11:40:03 +0200

    non funge..

--- snack-12/Js/script.js
+++ snack-12/Js/script.js
@@ -2,12 +2,16 @@ const word1 = prompt("Enter a word");
 const word2 = prompt("Enter another word");
 
 
-function wordLength(word1, word2){
-    if (word1.length = word2.length){
-        return (word1 + word2);
-    if (word1.length > word2.length)
-        return (word1);
-    else return (word2);
-}
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
+function sameLength(word1, word2) {
+    if (word1.length === word2.length) {
+        console.log(word1, word2);
+    } else {
+        if (word1.length > word2.length) {
+            console.log(word1);
+        } else {
+            console.log(word2);
+        }
+    }
 }
