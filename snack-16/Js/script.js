f1bf8d30f6dc1f5b131a97552e480c23b8f63e7e
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-22 12:38:52 +0200

    task completed

---
+++ snack-16/Js/script.js
@@ -0,0 +1,14 @@
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+
+const palla = {
+    nome: "palla",
+    peso: 10,
+}
+
+
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+
+const peso = prompt("Inserisci il peso della palla");
+palla.peso = peso;
